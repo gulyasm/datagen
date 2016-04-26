b033887c40baaef185b8fabc1e2d81bac164f93a
gulyasm <mgulyas86@gmail.com>
2016-04-26 22:29:26 +0200

    Lowered generate value.

--- color/generate_value.sh
+++ color/generate_value.sh
@@ -4,4 +4,4 @@ folder=~/stream-data
 data_folder=${folder}/color-value
 mkdir -p ${data_folder}
 
-rm -f ${data_folder}/* && python generate.py 1 1000 ${data_folder}
+rm -f ${data_folder}/* && python generate.py 1 100 ${data_folder}
